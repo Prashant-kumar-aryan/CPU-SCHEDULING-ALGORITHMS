867175180a04bddad4aa4884ccb16ddd0fa6413c
Prashant Kumar Aryan <135411392+Prashant-kumar-aryan@users.noreply.github.com>
2024-02-03 02:25:17 +0530

    Update non_premptive_priority_scheduling_using_heap.cpp

--- non_premptive_priority_scheduling_using_heap.cpp
+++ non_premptive_priority_scheduling_using_heap.cpp
@@ -1,3 +1,7 @@
+
+// Contributed BY Prashant Kumar Aryan
+// Mail me for any bugs found in this code -prashantkr2698@gmail.com
+// if u want to know the implementaion of this code u can use gpt for that i am not adding any comment lines
 #include <bits/stdc++.h>
 using namespace std;
 struct Process
